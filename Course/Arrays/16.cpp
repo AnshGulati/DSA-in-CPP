45b1d186daedd45d860604e035377225d41a9a48
Ansh Gulati <124679547+AnshGulati@users.noreply.github.com>
2025-07-13 16:43:36 +0530

    13-07-25

---
+++ Course/Arrays/16.cpp
@@ -0,0 +1,20 @@
+// Move Zeroes
+// https://leetcode.com/problems/move-zeroes/
+
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        int index = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != 0)
+            {
+                swap(nums[index], nums[i]);
+                index++;
+            }
+        }
+    }
+};
