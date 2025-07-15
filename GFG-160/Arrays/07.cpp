1a0fab68babbd60638fd3c9c63316f8327a2d1c6
Ansh Gulati <124679547+AnshGulati@users.noreply.github.com>
2025-07-15 23:45:29 +0530

    15-07-2025

---
+++ GFG-160/Arrays/07.cpp
@@ -0,0 +1,11 @@
+// Stock Buy and Sell – Multiple Transaction Allowed
+// https://www.geeksforgeeks.org/batch/gfg-160-problems/track/arrays-gfg-160/problem/stock-buy-and-sell2615
+
+class Solution
+{
+public:
+    int maximumProfit(vector<int> &prices)
+    {
+        // code here
+    }
+};
---
+++ GFG-160/Arrays/08.cpp
@@ -0,0 +1,11 @@
+// Stock Buy and Sell – Max one Transaction Allowed
+// https://www.geeksforgeeks.org/batch/gfg-160-problems/track/arrays-gfg-160/problem/buy-stock-2
+
+class Solution
+{
+public:
+    int maximumProfit(vector<int> &prices)
+    {
+        // code here
+    }
+};
