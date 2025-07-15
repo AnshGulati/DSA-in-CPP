b3824b1857a036a959c70580c7f282e8d04caa34
Ansh Gulati <124679547+AnshGulati@users.noreply.github.com>
2025-07-16 01:40:40 +0530

    15-07-25

--- GFG-160/Arrays/08.cpp
+++ GFG-160/Arrays/08.cpp
@@ -7,5 +7,19 @@ public:
     int maximumProfit(vector<int> &prices)
     {
         // code here
+
+        int bestBuy = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.size(); i++)
+        {
+            if (prices[i] > bestBuy)
+            {
+                maxProfit = max(maxProfit, prices[i] - bestBuy);
+            }
+            bestBuy = min(bestBuy, prices[i]);
+        }
+
+        return maxProfit;
     }
 };
